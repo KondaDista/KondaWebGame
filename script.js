2b81aae94b6858e346192e2f99dc8c7d62fe024d
Konda <Vaiper322@yandex.ru>
2023-04-24 20:02:43 +0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var score = 0, circles=[], colors = ["#E9967A","#90EE90","#BA55D3","#8B0000"];
 var canvas = document.getElementById('canvas');
 var paint = canvas.getContext('2d');
+var isPlay = false;
 
 canvas.height = window.innerHeight-17;
 canvas.width = window.innerWidth-17;
@@ -9,7 +10,7 @@ function MoveCircle(crcl)
 {
     paint.beginPath();
     paint.fillStyle=crcl.Color;
-    paint.arc(crcl.X, crcl.Y, 30, 0, Math.PI*2);
+    paint.arc(crcl.X, crcl.Y, crcl.Range, 0, Math.PI*2);
     paint.fill();
 }
 
@@ -18,7 +19,8 @@ function SpawnCircle(index)
     circles[index] = {
         X: 50 + Math.random() * (canvas.width - 100), 
         Y: -10 + Math.random() * 10, 
-        Color: getRandomColor()
+        Color: getRandomColor(),
+        Range: 20 + Math.random() * 30
     };
 }
 
@@ -31,29 +33,45 @@ function getRandomColor() {
     return color;
 }
 
+function Start() {
+    isPlay = true;
+    Update(isPlay);
+}
+
 function Update()
 {
-    paint.clearRect(0, 0, canvas.width, canvas.height);
-    paint.fillRect(0, canvas.height-5, canvas.width, 5);
-    document.getElementById("Count").textContent = "Score: " + score;
-    var index = circles.length;
-
-    if (Math.random() * 50 < 1) {
-        SpawnCircle(index)
-    }
-
-    for(var i = 0; i < circles.length; i++)
+    if(isPlay)
     {
-        if(++circles[i].Y < innerHeight - 100)
-        {       
-            MoveCircle(circles[i]);
+        paint.clearRect(0, 0, canvas.width, canvas.height);
+        paint.fillRect(0, canvas.height-5, canvas.width, 5);
+        document.getElementById("Count").textContent = "Score: " + score;
+        var index = circles.length;
+
+        if (Math.random() * 50 < 1) {
+            SpawnCircle(index)
         }
-        else
+
+        for(var i = 0; i < circles.length; i++)
         {
-            window.location.href='index.html'
+            if(++circles[i].Y < innerHeight - 100)
+            {       
+                MoveCircle(circles[i]);
+            }
+            else
+            {
+                isPlay = false;
+                let died = new Audio("END.mp3");
+                died.play();
+
+                paint.clearRect(0, 0, canvas.width, canvas.height);
+                circles.length = 0;
+                setTimeout(Start, 5000);
+                paint.font = "75px Tahoma";
+                paint.fillText("YOU LOSE", (window.innerWidth)/2.5, (window.innerHeight)/5);
+            }
         }
+        window.requestAnimationFrame(Update);
     }
-    window.requestAnimationFrame(Update);
 }
 
 canvas.addEventListener("mousedown",(click)=>
@@ -67,10 +85,12 @@ canvas.addEventListener("mousedown",(click)=>
 
         if (result <= 30) 
         {
+            let pop = new Audio("pop.mp3");
+            pop.play();
             score ++;
             circles.splice(circles.indexOf(cir), 1);
         }    
     });
 });
 
-Update(true);
+Start(true);
